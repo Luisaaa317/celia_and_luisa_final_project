cbf73fe63958ccdf99bc369f8c05394edacbdd89
Luisaaa317 <luise.kruse@gmail.com>
2026-08-14 11:28:48 +0200

    Update art_museum.py
    Change name of the game

--- art_museum.py
+++ art_museum.py
@@ -8,7 +8,7 @@ pygame.init()
 #set up window for game
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Buy Ticket Game")
+pygame.display.set_caption("Art Museum")
 
 #colors
 WHITE = (255, 255, 255)
